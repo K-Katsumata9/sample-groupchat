7b8fd1bef2d288f46fc3f50eb98d75c011d5d9f3
K-Katsumata9 <dowbler@icloud.com>
2024-03-30 15:31:44 +0900

    vue.jsプロジェクトの追加

---
+++ chat-vuejs/src/components/TestComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+      これはコンポーネントから表示されています。
+      <p>{{ message }}</p>
+      <button v-on:click="onClick">親メソッドを実行するボタン</button>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['message', 'isEnabled'],
+
+    methods: {
+        onClick() {
+            this.$emit('toggle')
+        }
+    }
+}
+</script>
+
